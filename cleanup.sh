6debc3709551a632d3fb5078cf31e6014e0a8ef0
cloudapps-idey <cloudnative-appdev@outlook.com>
2023-02-08 16:44:15 -0500

    initial checkin before jolokia

---
+++ cleanup.sh
@@ -0,0 +1,7 @@
+oc delete secret quarkus-todo-app-git-github-webhook-secret
+oc delete secret quarkus-todo-app-git-generic-webhook-secret
+oc delete bc quarkus-todo-app-git
+oc delete is quarkus-todo-app-git
+oc delete route quarkus-todo-app-git
+oc delete svc quarkus-todo-app-git
+oc delete deployment quarkus-todo-app-git
